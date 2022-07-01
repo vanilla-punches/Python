3af142f71964497507f6f33e36f10d4db62a8b41
Ian Lee <>
2022-07-01 14:40:59 -0400

    test

--- test.py
+++ test.py
@@ -4,8 +4,16 @@ age = 4
 
 
 print("My " + animal + "name is " + name)
-print("She is " + str(age) + " years old")
+print("She is " + str(age) + " years old") #+ needs to specify class
 
 
 print("My ", animal, "name is ", name)
-print("She is ", age, " years old")
+print("She is ", age, " years old") #comma doesn't need to specific class
+
+
+'''We can write mutiple lines(comments) when we use 
+Like this we can write it
+Here we go'''
+
+# command + / will add or remove comments
+
