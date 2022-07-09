2f3372762e4957399bd70e9da91e0e8d7de39988
Ian Lee <vanilla.punches@gmail.com>
2022-07-09 17:20:02 -0400

    module

---
+++ lab_24.py
@@ -0,0 +1,13 @@
+from tkinter import PROJECTING
+import theater_module as mv
+mv.price(3)
+mv.senior_price(5)
+mv.children_price(4)
+
+from theater_module import *
+price(3)
+senior_price(6)
+children_price(4)
+
+from theater_module import children_price as price
+price(6)
---
+++ theater_module.py
@@ -0,0 +1,12 @@
+
+# Regular Price
+def price(people):
+    print("Regular Price for {0} is {1}".format(people, people * 10))
+
+# Senior Price
+def senior_price(people):
+    print("Senior Price for {0} is {1}".format(people, people * 5))
+
+# Children price
+def children_price(people):
+    print("Children Price for {0} is {1}".format(people, people * 7))
