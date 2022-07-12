06f185d25427dd663ec497b119c53f0773c08904
Ian Lee <vanilla.punches@gmail.com>
2022-07-11 22:02:33 -0400

    [Game 2] 1. background, stage, character

---
+++ pygame_project/1_frame_background_stage_character.py
@@ -0,0 +1,61 @@
+import pygame
+import os
+################################################################################################################
+# Initialization (Codes must be done)
+pygame.init() # initalization
+
+# size of display
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# title of screen
+pygame.display.set_caption("Game 2")
+
+# FPS
+clock = pygame.time.Clock()
+
+################################################################################################################
+# 1. Background, Game image(characters), Location, Speed, Font
+current_path = os.path.dirname(__file__) # return current file directory
+image_path = os.path.join(current_path, "images") # return images folder
+
+# Creating background
+background = pygame.image.load(os.path.join(image_path, "background.png"))
+
+# Creating stage
+stage = pygame.image.load(os.path.join(image_path, "stage.png"))
+stage_size = stage.get_rect().size
+stage_height = stage_size[1] # We do this to make the character on top of the stage
+
+# Creating character
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height - stage_height
+
+# Event loop
+running = True # Is game running?
+while running:
+    dt = clock.tick(60) # frame per second
+    # print("fps: " + str(clock.get_fps()))
+
+    # 2. Event operations(keyboard, mouse)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # 3. Game character location
+
+    # 4. Collision
+
+    # 5. Draw on screen
+    screen.blit(background, (0, 0))
+    screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
+
+    pygame.display.update() # repeat drawing background
+
+pygame.quit() # pygame ends
